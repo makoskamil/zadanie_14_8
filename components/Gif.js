a68e7faadc2c28766da33ade745a67d53db2080e
Kamil Makoś <makos.kamil@o2.pl>
2018-06-03 21:01:20 +0200

    change files

--- components/Gif.js
+++ components/Gif.js
@@ -20,3 +20,4 @@ Gif = React.createClass({
     );
   }
 });
+
--- components/Search.js
+++ components/Search.js
@@ -14,6 +14,7 @@ Search = React.createClass({
             value={this.state.searchTerm}
             />
     }
+    <Search onSearch={this.handleSearch}/>
 });
     
 getInitialState(){
@@ -49,3 +50,33 @@ render: function() {
                 value={this.state.searchTerm}
             />
 }
+
+handleSearch: function(searchingText) {  // 1.
+    this.setState({
+      loading: true  // 2.
+    });
+    this.getGif(searchingText, function(gif) {  // 3.
+      this.setState({  // 4
+        loading: false,  // a
+        gif: gif,  // b
+        searchingText: searchingText  // c
+      });
+    }.bind(this));
+  },
+      
+getGif: function(searchingText, callback) {  // 1.
+    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+    var xhr = new XMLHttpRequest();  // 3.
+    xhr.open('GET', url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+           var data = JSON.parse(xhr.responseText).data; // 4.
+            var gif = {  // 5.
+                url: data.fixed_width_downsampled_url,
+                sourceUrl: data.url
+            };
+            callback(gif);  // 6.
+        }
+    };
+    xhr.send();
+},
