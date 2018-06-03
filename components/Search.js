536f60a8f7f3cb3b866919841fe5005b147c293f
Kamil Makoś <makos.kamil@o2.pl>
2018-06-03 19:33:16 +0200

    add_files

---
+++ components/Search.js
@@ -0,0 +1,51 @@
+Search = React.createClass({
+    render: function() {
+        var styles = {
+            fontSize: '1.5em',
+            width: '90%',
+            maxWidth: '350px'
+        };
+        
+    return <input
+            type="text"
+            onChange={this.handle.Change}
+            placeholder="Tutaj wpisz wyszukiwaną frazę"
+            style={styles}
+            value={this.state.searchTerm}
+            />
+    }
+});
+    
+getInitialState(){
+    return {
+        searchingText: ''
+    };
+},
+
+handleChange: function(event) {
+    var searchingText = event.target.value;
+    this.setState({
+        searchingText: searchingText});
+    if (searchingText.length > 2) {
+        this.props.onSearch(searchingText);
+    }
+},
+
+handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+        this.props.onSearch(this.state.searchingText);
+    }
+},
+    
+render: function() {
+    var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
+    
+    return <input 
+                type="text"
+                onChange={this.handleKeyUp}
+                onKeyUp={this.handleKeyUp}
+                placeholder="Tutaj wpisz wyszukiwaną frazę"
+                style={styles}
+                value={this.state.searchTerm}
+            />
+}
